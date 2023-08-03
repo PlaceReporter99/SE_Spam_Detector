78928be7bf2f4e95e0820d416df14718eddb8533
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-03 19:14:02 +0100

    Update spamsearch.py

--- spamsearch.py
+++ spamsearch.py
@@ -1,10 +1,11 @@
 import requests
+import re
 
 def get_spam(site: str = "stackoverflow") -> str:
   true = True
   false = False
   json = eval(requests.get(f"https://api.stackexchange.com/2.3/questions?page=1&pagesize=30&order=desc&sort=creation&site={site}&filter=!Fc6b9*tI0e7rqpMIoRUAgp8tJ8").text)['items']
-  spam = [x for x in json if re.search(r'(?:\d-*){10}',x['link'])]
+  spam = [x for x in json if re.search(r'(?:\d-*){10}', x['link'])]
   return '\n'.join(x['link'] for x in spam)
 
 def actout(name: str, value: str) -> None:
